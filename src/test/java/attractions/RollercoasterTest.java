8c8ba882196a926000f9663042043ce6dc0104b2
Ahmed Hussein <ahmd.adl.elsayed@gmail.com>
2019-10-15 22:11:34 +0100

    RollerCoaster ISecurity

--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -2,16 +2,23 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
+    Visitor visitor1;
+    Visitor visitor2;
+    Visitor visitor3;
 
     @Before
     public void setUp() {
         rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+        visitor1 = new Visitor(13, 140, 40);
+        visitor2 = new Visitor(13, 170, 50);
+        visitor3 = new Visitor(11, 150, 70);
     }
 
     @Test
@@ -27,5 +34,12 @@ public class RollercoasterTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
+    }
+
+    @Test
+    public void testRollerCoasterAllowedToVisitors(){
+        assertEquals(false, rollerCoaster.isAllowedTo(visitor1));
+        assertEquals(true, rollerCoaster.isAllowedTo(visitor2));
+        assertEquals(false, rollerCoaster.isAllowedTo(visitor3));
     }
 }
