4a4a6089491893bfb4c74310bac010389effd453
Ahmed Hussein <ahmd.adl.elsayed@gmail.com>
2019-10-15 22:22:09 +0100

    Stalls and attraction IReviewed

--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -14,7 +14,7 @@ public class TobaccoStallTest {
 
     @Before
     public void setUp() throws Exception {
-        tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1, 7);
         visitor1 = new Visitor(17, 180, 60);
         visitor2 = new Visitor(20, 175, 45);
     }
