fdf317eff78a48dd48b60650b1ab12caccdec9d0
Ahmed Hussein <ahmd.adl.elsayed@gmail.com>
2019-10-15 21:55:33 +0100

    tobacco stall ISecurity

--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -2,16 +2,21 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class TobaccoStallTest {
 
     TobaccoStall tobaccoStall;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        visitor1 = new Visitor(17, 180, 60);
+        visitor2 = new Visitor(20, 175, 45);
     }
 
     @Test
@@ -27,5 +32,11 @@ public class TobaccoStallTest {
     @Test
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void testVisitorAllowedToTobaccoStall() {
+        assertEquals(false, tobaccoStall.isAllowedTo(visitor1));
+        assertEquals(true, tobaccoStall.isAllowedTo(visitor2));
     }
 }
