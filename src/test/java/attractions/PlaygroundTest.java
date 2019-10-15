e2b878dabf740b3dc22ec084917bd4b104388e9d
Ahmed Hussein <ahmd.adl.elsayed@gmail.com>
2019-10-15 21:49:58 +0100

    playground ISecurity

--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -2,15 +2,20 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class PlaygroundTest {
     Playground playground;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         playground = new Playground("Fun Zone", 7);
+        visitor1 = new Visitor(14,165, 30);
+        visitor2 = new Visitor(19,185, 130);
     }
 
     @Test
@@ -26,5 +31,11 @@ public class PlaygroundTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, playground.getVisitCount());
+    }
+
+    @Test
+    public void visitorAllowedToEnterPlayground(){
+        assertEquals(true, playground.isAllowedTo(visitor1));
+        assertEquals(false, playground.isAllowedTo(visitor2));
     }
 }
