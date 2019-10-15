2cb7a952a19df888e14dc02bf584e1e0c7121f16
Ahmed Hussein <ahmd.adl.elsayed@gmail.com>
2019-10-15 22:32:46 +0100

    rollerCoaster ITicketed

--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -17,7 +17,7 @@ public class RollercoasterTest {
     public void setUp() {
         rollerCoaster = new RollerCoaster("Blue Ridge", 10);
         visitor1 = new Visitor(13, 140, 40);
-        visitor2 = new Visitor(13, 170, 50);
+        visitor2 = new Visitor(13, 205, 50);
         visitor3 = new Visitor(11, 150, 70);
     }
 
@@ -41,5 +41,13 @@ public class RollercoasterTest {
         assertEquals(false, rollerCoaster.isAllowedTo(visitor1));
         assertEquals(true, rollerCoaster.isAllowedTo(visitor2));
         assertEquals(false, rollerCoaster.isAllowedTo(visitor3));
+    }
+
+    @Test
+    public void testPriceForVisitors() {
+        assertEquals(rollerCoaster.defaultPrice(), rollerCoaster.priceFor(visitor1), 0);
+        assertEquals(8.4, rollerCoaster.priceFor(visitor1), 0);
+        assertEquals(2 * rollerCoaster.defaultPrice(), rollerCoaster.priceFor(visitor2), 0);
+        assertEquals(16.8, rollerCoaster.priceFor(visitor2), 0);
     }
 }
